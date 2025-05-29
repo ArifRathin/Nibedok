5e6ceeff2d1c042e06732a3eb789da5a3e6dc3fb
Arif Rathin <arif.rathin@gmail.com>
2025-05-30 01:00:12 +0600

    changed base_dir code

--- nibedok/settings.py
+++ nibedok/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from os.path import normpath, join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -133,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = normpath(join(BASE_DIR, 'staticfiles'))
 
 STATIC_URL = 'static/'
 
