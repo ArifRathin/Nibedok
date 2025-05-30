0243004f695b2e0bd6ab86714f39848cf86259cd
Arif Rathin <arif.rathin@gmail.com>
2025-05-30 20:03:52 +0600

    daphne solution

--- nibedok/settings.py
+++ nibedok/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import django
+django.setup()
 from pathlib import Path
 import os
 from os.path import normpath, join
