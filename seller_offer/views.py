bad16ccbe36dc61b2ba6c227a923b2d6ca8c1401
Arif Rathin <arif.rathin@gmail.com>
2025-06-05 01:20:18 +0600

    added error tracer at seller offer

--- seller_offer/views.py
+++ seller_offer/views.py
@@ -94,7 +94,7 @@ def createSellerOffer(request):
                 messages.success(request,"Successfully sent!", extra_tags="success-sent")
                 return redirect("show-create-seller-offer",post_code_name)
         except Exception as e:
-            # return HttpResponse(str(e))
+            return HttpResponse(str(e))
             messages.error(request, "Something went wrong. Please check your input values.", extra_tags="failed")
             return redirect("show-create-seller-offer",post_code_name)
         
